3a393950bc05e58024793f051280b141c4722230
SofiaAlmeida <sofiil97@gmail.com>
2020-02-25 10:31:05 +0100

    P0: completa ejercicio 2

--- P0/p0.py
+++ P0/p0.py
@@ -5,8 +5,10 @@
 from sklearn import datasets
 import seaborn as sns; sns.set()
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import StratifiedShuffleSplit
+import math
 
 def ej1():
     # Leemos la base de datos iris de scikit-learn
@@ -37,13 +39,41 @@ def ej2():
     # Leemos la base de datos iris de scikit-learn
     iris = datasets.load_iris()
     print("Cargada la base de datos iris")
+    X = iris.data
+    y = iris.target
+
     
     # Separamos en training (80% de los datos) y test (20%) aleatoriamente y conservando la proporción de elementos en cada clase
     sss = StratifiedShuffleSplit(n_splits=1, random_state=12345, test_size=0.2)
-    index = sss.split(iris.data, iris.target)
-    print(index[0], index[1])
-    print("Training: ", index.train, "Test: ", index.test)
+  
+    for train_index, test_index in sss.split(X, y):
+        print("índices de los elementos de cada conjunto:")
+        print("TRAIN:", train_index, "TEST:", test_index)
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+
     
+    print("Porcentaje clase 0 en train: ", np.count_nonzero(y_train == 0) / y_train.shape[0] * 100, "%")
+    print("Porcentaje clase 0 en test: ", np.count_nonzero(y_test == 0) / y_test.shape[0] * 100, "%", "\n")
+    print("Porcentaje clase 1 en train: ", np.count_nonzero(y_train == 1) / y_train.shape[0] * 100, "%")
+    print("Porcentaje clase 1 en test: ", np.count_nonzero(y_test == 1) / y_test.shape[0] * 100, "%", "\n")
+    print("Porcentaje clase 2 en train: ", np.count_nonzero(y_train == 2) / y_train.shape[0] * 100, "%")
+    print("Porcentaje clase 2 en test: ", np.count_nonzero(y_test == 2) / y_test.shape[0] * 100, "%", "\n")
+
+def ej3():
+    # Obtener 100 valores equiespaciados entre 0 y 2pi
+    values = np.linspace(0, 2*math.pi, 100)
+    print(values)
+    print("Número de elementos: ", values.shape[0])
+
+    # Obtener el valor de sin(x)
+
+    # Obtener el valor de cos(x)
+
+    # Obtener el valor de sin(x) + cos(x)
+
+    # Visualizar las tres curvas simultáneamente en el mismo plot (líneas discontinuas en negro, azul y rojo
     
 #ej1()
-ej2()
+#ej2()
+ej3()
